02d5f9c4a074b292d62c95ef2e1546dcf75c6ffc
Lazken <bram.borremans@bulls-i.company>
2024-12-14 12:21:40 +0100

    adding athumi session

--- tmux/.tmux/plugins/tmuxifier/layouts/athumi.session.sh
+++ tmux/.tmux/plugins/tmuxifier/layouts/athumi.session.sh
@@ -1,41 +1,34 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
 #session_root "~/Projects/default"
-session_root ~
+session_root ~/repos/athumi
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "brambo"; then
-  window_root "~"
-  new_window "local"
-  
-  window_root "~/repos/vlaanderen"
-  new_window "VO"
-  run_cmd "nvim"
-  split_v 20
-  select_pane 1
-
+if initialize_session "athumi"; then
   window_root "~/repos/athumi"
-  new_window "ATHUMI"
+  new_window "ROOT"
   run_cmd "nvim"
   split_v 20
   select_pane 1
 
-  window_root "~/repos/bullsi"
-  new_window "Bullsi"
+  window_root "~/repos/athumi/CPT-Azure-infra"
+  new_window "CPT-Azure-infra"
   run_cmd "nvim"
   split_v 20
   select_pane 1
-
-  window_root "~/repos/noro"
-  new_window "Noro"
+  
+  window_root "~/repos/athumi/application-definitions.cpt"
+  new_window "appdef-CPT"
   run_cmd "nvim"
   split_v 20
   select_pane 1
-  split_h 40
-  run_cmd "k9s"
+ 
+  window_root "~/repos/athumi/application-definitions.ess"
+  new_window "appdef-ESS"
+  run_cmd "nvim"
+  split_v 20
   select_pane 1
-
 
 
   # Create a new window inline within session layout definition.
