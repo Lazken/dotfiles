5f49a1b06956a1fe0b4645aff70ec1949bb519d8
Bram Borremans <bramborremans@gmail.com>
2026-04-15 21:38:37 +0200

    Updating desktop setting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,15 +2,35 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
+    branch = 'main',
     build = ":TSUpdate",
-    config = function()
-      local config = require("nvim-treesitter.config")
-      config.setup({
-        auto_install = true,
-        sync_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = { 'terraform', 'hcl', 'gdscript', 'yaml', 'markdown', 'regex', 'bash', 'python', 'helm' },
+    init = function()
+      local parser_installed = {
+        'terraform',
+        'hcl',
+        'gdscript',
+        'yaml',
+        'markdown',
+        'regex',
+        'bash',
+        'python',
+        'helm',
+      }
+
+      vim.defer_fn(function() require("nvim-treesitter").install(parser_installed) end, 1000)
+      require("nvim-treesitter").update()
+      -- auto-start highlights & indentation
+      vim.api.nvim_create_autocmd("FileType", {
+        desc = "User: enable treesitter highlighting",
+        callback = function(ctx)
+          -- highlights
+          local hasStarted = pcall(vim.treesitter.start) -- errors for filetypes with no parser
+          -- indent
+          local noIndent = {}
+          if hasStarted and not vim.list_contains(noIndent, ctx.match) then
+            vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end
   }
